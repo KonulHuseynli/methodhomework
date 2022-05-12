9488214d39f56f9f33d25aec6537fb6a6151c60b
KonulHuseynli <konulrh@code.edu.az>
2022-05-13 02:11:50 +0400

    added newtask method

---
+++ TASK1Method.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("include value : ");
+            int a = Int32.Parse(Console.ReadLine());
+            IsPrime(a);
+        }
+        static void IsPrime(int a)
+        {
+            for (int counter = 2; counter < a; counter++)
+            {
+                if (a % counter == 0  )
+                {
+                    Console.WriteLine("number is not simple");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("number is simple");
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ TASK2Method.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 92, 87, 76, 65, 54, 43, 32,3};
+            CalcAvg(arr);
+        }
+        static void CalcAvg(int[] arr)
+        {
+            int sum = 0;
+            int NumericalAverage = 0;
+            int counter = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+                counter++;
+                NumericalAverage = sum / counter;
+            }
+            Console.WriteLine(NumericalAverage);
+            if (NumericalAverage>60)
+                {
+                    Console.WriteLine(" you graduated");
+                }
+                else
+                {
+                    Console.WriteLine( "you arent graduated");
+                }
+            }
+           
+
+        }
+    }
+
